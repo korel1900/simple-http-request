94e296cf842ea2c60da62ad08daa95667437482a
Korel_1900 <161307697+korel1900@users.noreply.github.com>
2024-03-16 17:46:31 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+**simple-http-request**
+
+A simple way to make http requests using only one input object
+
+**Install**
+
+`npm install simple-http-request`
---
+++ index.js
@@ -0,0 +1,53 @@
+const axios = require('axios');
+
+async function request(input){
+    let startTime, endTime;
+    startTime = new Date();
+
+    let response = {
+        httpHeader:0,
+        timeInMilisec:0,
+        data:{}
+    };
+
+    const url=input.domain+"/"+input.path
+    if(input.httpMethod == "GET"){
+        await axios.get(url, {
+            params: input.params,
+            headers: input.headers
+        })
+        .then(function (res) {
+            response.data = res.data
+            response.httpHeader = res.status
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });  
+    }
+    else if(input.httpMethod == "POST"){
+        await axios.post(url, {
+            data: input.body,
+            headers: input.headers
+        })
+        .then(function (res) {
+            response.data = res.data
+            response.httpHeader = res.status
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
+
+    }
+    endTime = new Date();
+    response.timeInMilisec = endTime - startTime; 
+
+    return response;
+}
+
+module.exports = request;
